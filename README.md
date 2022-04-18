54f0e8775c31da2c55e36772f289b66dee7de2f1
sandertyu <noasanders@ucdavis.edu>
2022-04-18 16:56:28 -0700

    v1.0.1

---
+++ README.md
@@ -0,0 +1,8 @@
+Overloads the tactical borders and target cursor used in the "Modern" graphic mode with those used in the "Ascii" graphic mode.
+
+I just like how it looks :)
+
+https://github.com/sandertyu/tome-ascii-borders
+
+---Changelog---
+- v1.0.1 Initial release
---
+++ init.lua
@@ -0,0 +1,24 @@
+long_name = "Ascii Tactical Borders"
+short_name = "ascii-borders" 
+for_module = "tome"
+version = {1,7,4}
+addon_version = {1,0,1}
+weight = 100
+author = {'lowfye'}
+homepage = "https://github.com/sandertyu/tome-ascii-borders"
+description = [[
+Overloads the tactical borders and target cursor used in the "Modern" graphic mode with those used in the "Ascii" graphic mode.
+
+I just like how it looks :)
+
+https://github.com/sandertyu/tome-ascii-borders
+
+---Changelog---
+- v1.0.1 Initial release
+]]
+tags = {'ascii', 'borders', 'tactical'}
+
+overload = true
+superload = false
+data = false
+hooks = false
