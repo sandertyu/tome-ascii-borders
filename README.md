d67c52f060da3782590c169f51ebbb103613a0ee
sandertyu <noasanders@ucdavis.edu>
2022-04-21 14:28:39 -0700

    v1.1.0

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 Overloads the tactical borders and target cursor used in the "Modern" graphic mode with those used in the "Ascii" graphic mode.
+The images used here were contributed by Rexorcorum and are 64x64 instead of the default 32x32, to look better on larger tile sizes.
 
 I just like how it looks :)
 
 https://github.com/sandertyu/tome-ascii-borders
 
 ---Changelog---
+- v1.1.0 Replaced 32x32 images with 64x64
 - v1.0.1 Initial release
--- init.lua
+++ init.lua
@@ -2,18 +2,20 @@ long_name = "Ascii Tactical Borders"
 short_name = "ascii-borders" 
 for_module = "tome"
 version = {1,7,4}
-addon_version = {1,0,1}
+addon_version = {1,1,0}
 weight = 100
 author = {'lowfye'}
 homepage = "https://github.com/sandertyu/tome-ascii-borders"
 description = [[
 Overloads the tactical borders and target cursor used in the "Modern" graphic mode with those used in the "Ascii" graphic mode.
+The images used here were contributed by Rexorcorum and are 64x64 instead of the default 32x32, to look better on larger tile sizes.
 
 I just like how it looks :)
 
 https://github.com/sandertyu/tome-ascii-borders
 
 ---Changelog---
+- v1.1.0 Replaced 32x32 images with 64x64
 - v1.0.1 Initial release
 ]]
 tags = {'ascii', 'borders', 'tactical'}
