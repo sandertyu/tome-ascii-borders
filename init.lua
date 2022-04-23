4afbc74558179b5aa88a485fb4018e79d8e7a88b
sandertyu <noasanders@ucdavis.edu>
2022-04-23 01:45:37 -0700

    updated 2.0.0 descriptions

--- init.lua
+++ init.lua
@@ -7,14 +7,15 @@ weight = 200
 author = {'lowfye'}
 homepage = "https://github.com/sandertyu/tome-ascii-borders"
 description = [[
-Overloads the tactical borders and target cursor used in the "Modern" graphic mode with those used in the "Ascii" graphic mode.
+Adds an option to the UI tab in Game Options where you can choose a style to overload the tactical borders and target cursor used in the "Modern" graphic mode with those used in the "Ascii" graphic mode.
 
 I just like how it looks :)
 
 https://github.com/sandertyu/tome-ascii-borders
 
 ---Changelog---
-- v1.0.1 Initial release
+- v2.0.0 Game option with 3 ascii border style options. The border style used in v1.0.1 is now called "Official".
+- v1.0.1 Initial release.
 ]]
 tags = {'ascii', 'borders', 'tactical', 'frame', 'outline'}
 
